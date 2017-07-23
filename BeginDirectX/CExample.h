01353242adc838b76b4a13c436d148fce37cceb5
zero <nguyenhieu01673@gmail.com>
2017-07-23 20:18:05 +0700

    restart

---
+++ BeginDirectX/CExample.h
@@ -0,0 +1,30 @@
+////Test
+//
+//#ifndef __CEXAMPLE_H__
+//#define __CEXAMPLE_H__
+//
+//
+//#include "CGame.h"
+//
+//
+//using namespace Zero;
+//
+//
+//class CExample : public CGame
+//{
+//
+//
+//public:
+//	CExample(void);
+//	virtual ~CExample(void);
+//
+//
+//protected:
+//	bool Init(void);
+//	void Destroy(void);
+//
+//	
+//};
+//
+//
+//#endif
--- zero/zero.h
+++ zero/zero.h
@@ -1,6 +1,11 @@
-#ifndef __ZERO_H__
-#define __ZERO_H__
-
-#include <stdio.h>
-
-#endif
+//#ifndef __ZERO_H__
+//#define __ZERO_H__
+//
+//
+//
+//#include "CGame.h"
+//#include "CStateManagement.h"
+//#include "CState.h"
+//#include "CTimer.h"
+//
+//#endif
